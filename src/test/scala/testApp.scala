2a907fc28001f3d1e5daee242bf24a82d1945e9e
ukko miettinen <ukko.miettinen@aalto.fi>
2021-04-04 20:07:41 +0300

    testApp added as a testing GUI

--- src/test/scala/testApp.scala
+++ src/test/scala/testApp.scala
@@ -1,9 +1,27 @@
 
 
 import scalafx.application.JFXApp
+import scalafx.scene._
+import scalafx.scene.shape._
+import scalafx.scene.effect._
+import scalafx.scene.paint._
+import scalafx.event._
 
 
 object testApp extends JFXApp {
 
-
+  stage = new JFXApp.PrimaryStage {
+    title = "testApp"
+    scene = new Scene(1700,900) {
+      fill = Color.rgb(140,140,140)
+      val circle = new Circle()
+      circle.centerX = 850
+      circle.centerY = 450
+      circle.radius = 30
+      circle.fill = Color.rgb(240,240,240)
+      content = List(circle)
+    }
+  }
 }
+
+
